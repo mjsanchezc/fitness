eac269e0d64c3d3e68ef6e6eadf43e450fd1d791
Maria Jose Sanchez Cairazco <mariajose.sanchezcairazco@gmail.com>
2025-09-15 15:36:49 -0500

    Added search field

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,7 +8,70 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _searchField(),
+        ],
+      ),
     );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: EdgeInsets.only(top: 40, left: 30, right: 30),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xff1D1617).withValues(alpha: 0.07),
+                blurRadius: 40,
+                spreadRadius: 0.0,
+                offset: Offset(0.0, 10.0)
+              )
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: 'Search Pancake',
+              hintStyle: TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.only(top: 12, bottom: 12, left: 20, right: 20),
+                child: SvgPicture.asset('assets/icons/Search.svg'),
+              ),
+              suffixIcon: SizedBox(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Color(0xffDDDADA),
+                        thickness: 0.5,
+                        indent: 15,
+                        endIndent: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5, top: 12, bottom: 12, right: 15),
+                        child: SvgPicture.asset('assets/icons/Filter.svg'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none
+              )
+            ),
+          ),
+        );
   }
 
   AppBar appBar() {
